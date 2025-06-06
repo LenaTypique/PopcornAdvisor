98f7fc9fe5fa56712f0d304efffc1928f25597f4
Marie Pawlowski <marie.pawlowski@student-cs.fr>
2025-06-06 12:53:00 +0200

    page about

--- frontend/src/pages/About/About.jsx
+++ frontend/src/pages/About/About.jsx
@@ -1,7 +1,15 @@
 import './About.css';
 
 function About() {
-  return <div className="about">about page</div>;
+  return (
+    <div className="about">
+      <h1>Qui sommes-nous ?</h1>
+      <p>
+        Ce projet a été mené par Jean Clenet, Marie Pawlowski et Léna
+        Pohier--Plantel dans le cadre du projet d'EI en partenariat avec Théodo.
+      </p>
+    </div>
+  );
 }
 
 export default About;
